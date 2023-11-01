76de5767abea9328a87eb7f3bc160ab9d7c8792c
Tatiana Yurchenko <taf88156@mail.ru>
2023-11-01 16:58:59 +0300

    feat: add tests on www.saucedemo.com redrover denis

--- test_den_lectures_07.py
+++ test_den_lectures_07.py
@@ -2,20 +2,22 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+
 import time
 
 
+def login(driver):
+    driver.get("https://www.saucedemo.com/")
+    time.sleep(2)
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
 def test_add_item_to_cart(driver, wait):
-    # driver.get("https://www.saucedemo.com/")
-    # username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    # username_field.send_keys("standard_user")
-    #
-    # password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    # password_field.send_keys("secret_sauce")
-    #
-    # login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    # login_button.click()
-    a = login()
+    login(driver)
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
     driver.find_element(By.CSS_SELECTOR, 'button[id="add-to-cart-sauce-labs-backpack"]').click()
@@ -24,21 +26,13 @@ def test_add_item_to_cart(driver, wait):
     item = driver.find_element(By.CSS_SELECTOR, 'div[class="cart_quantity"]')
     assert item.text == '1'
 
-    driver.quit()
 
 def test_loguot(driver, wait):
-    driver.get("https://www.saucedemo.com/")
-    url_before = driver.current_url
-    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    username_field.send_keys("standard_user")
-    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    password_field.send_keys("secret_sauce")
-
-    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    login_button.click()
+    login(driver)
     wait.until(EC.element_to_be_clickable((By.ID, 'react-burger-menu-btn'))).click()
 
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#logout_sidebar_link'))).click()
-
     url_after = driver.current_url
-    assert url_before == url_after
+    assert url_after == "https://www.saucedemo.com/"
+
+    driver.page_source
